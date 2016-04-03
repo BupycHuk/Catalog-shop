bfbdbbbc39268c9154a979a7e7da9f3c8e649966
Mahabbat Munalbayeva <mahabbat1011@gmail.com>
2016-04-03 12:47:49 +0600

    Добавление продукта

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,45 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
-		respond_to do |format|
-			format.json { render json: @categories}
-			format.html
-		end
+    respond_to do |format|
+      format.json { render json: @categories }
+      format.html
+    end
   end
 
-	def new
-	end
+  def show
+    @category = Category.find(params[:id])
+  end
 
-	def create
-		@category = Category.new(params.require(:category).permit(:title))
-		@category.save
-		redirect_to @category
-	end
+  def new
+    @category = Category.new
+  end
 
-	def show
-	 	@category = Category.find(params[:id])
-	 end
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to categories_path
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to @category
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,20 +3,33 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     respond_to do |format|
-      format.json { render json: @products}
+      format.json { render json: @products }
       format.html
     end
   end
 
   def show
-  	@product = Product.find(params[:id])
+    @product = Product.find(params[:id])
+  end
 
+  def new
+    @product = Product.new
   end
 
   def create
-    @category = Category.find(params[:category_id])
-    @product = @category.products.create(product_params)
-    redirect_to category_path(@category)
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to @product
   end
 
   def product_item
@@ -24,14 +37,13 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:category_id])
-    @product = @category.products.find(params[:id])
+    @product = Product.find(params[:id])
     @product.destroy
-    redirect_to category_path(@category)
+    redirect_to products_path
   end
 
-   private
-    def product_params
-      params.require(:product).permit(:title, :price, :text, :picture)
-    end
+  private
+  def product_params
+    params.require(:product).permit(:title, :price, :text, :picture, :category_id)
+  end
 end
